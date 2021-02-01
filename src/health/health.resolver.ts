a449e929fe128562c7b9e62c9551a25dc443a850
zabozlaev <hunter.eal@yandex.ru>
2021-02-01 17:42:05 +0000

    Update health.resolver.ts

--- src/health/health.resolver.ts
+++ src/health/health.resolver.ts
@@ -1,14 +1,11 @@
 import { Query, Resolver } from '@nestjs/graphql';
-import { RedisPubSub } from 'graphql-redis-subscriptions';
-import { InjectPubSub } from '../pubsub';
+
 import { HealthService } from './health.service';
 
 @Resolver()
 export class HealthResolver {
   constructor(
     private readonly healthService: HealthService,
-    @InjectPubSub()
-    private readonly _pubsub: RedisPubSub,
   ) {}
 
   // replace <x> with service name
